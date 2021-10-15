7f49cfe5d0245ad899237f6bc6be6a03e31dc6a7
SCOPXTOKEN <86366071+SCOPXToken@users.noreply.github.com>
2021-10-15 16:16:31 +0800

    Update hardhat.config.js

--- scopxnft-dapps/hardhat.config.js
+++ scopxnft-dapps/hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
- const privateKey = "6ced5e6142cd403903a25f4ac26f060354f92677a9ca2e7a9eae9641ca06b390";
+ const privateKey = "";
 
 module.exports = {
   networks: {
